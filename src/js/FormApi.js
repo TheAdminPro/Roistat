15cf3a98a0d3531c5e62819fcdd0a884a0f982e7
TheAdminPro <45514836+TheAdminPro@users.noreply.github.com>
2020-07-13 23:36:25 +0300

    form api added

---
+++ src/js/FormApi.js
@@ -0,0 +1,24 @@
+class Form {
+  constructor(selector){
+    this.form = document.querySelector(selector); 
+  }
+
+  onSubmitEvent(callback){
+    this.form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      const dataFields = this.getValueFields();
+      callback(dataFields);
+    });
+  }
+  // Get Value From Form's Input
+  getValueFields(){
+    const fields = this.form.querySelectorAll('.app-form-control');
+
+    return [...fields].reduce((acc, field) => {
+      acc[field.dataset.type] = field.value.trim();
+      return acc;
+    }, {});
+  }
+}
+
+export default Form;
